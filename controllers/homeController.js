d0fa6b6d0274e5588949b26a2885359979276ccf
Aphideth <59237952+Aphideth@users.noreply.github.com>
2021-12-15 09:47:42 +0100

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // Dans ce controller, toutes les routes commencent par /home cf(routes/routings.js L:8)
 router.get('/', async (req, res) => {
-    req.status(200).json({message : 'Hello World'});
+    res.status(200).json({message : 'Hello World'});
 })
 
 export default router;
